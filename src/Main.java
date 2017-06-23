a94c3ae47585b0d102be94f6b39cda27ba54b63c
kyarkova <ksenia.yarkova@gmail.com>
2017-06-23 23:27:12 +0300

    1

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-import com.sun.org.apache.xpath.internal.SourceTree;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-        System.out.println(123);
-    }
-}
--- src/sleepIn.java
+++ src/sleepIn.java
@@ -2,4 +2,9 @@
  * Created by kseni on 23.06.2017.
  */
 public class sleepIn {
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        if (!weekday||vacation)
+            return true;
+        else return false;
+    }
 }
