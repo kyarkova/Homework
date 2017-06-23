ec4e1d76ce5b6b538a336c86fe3855b493153613
kyarkova <ksenia.yarkova@gmail.com>
2017-06-23 23:29:16 +0300

    1

--- src/sleepIn.java
+++ src/sleepIn.java
@@ -3,7 +3,7 @@
  */
 public class sleepIn {
     public boolean sleepIn(boolean weekday, boolean vacation) {
-        if (!weekday||vacation)
+        if (!weekday || vacation)
             return true;
         else return false;
     }
