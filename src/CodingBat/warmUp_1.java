ac2e75bfc8a73c15876e7b29e5d14a6e6fe00c71
kyarkova <ksenia.yarkova@gmail.com>
2017-06-24 11:41:27 +0300

    1

--- src/CodingBat/warmUp_1.java
+++ src/CodingBat/warmUp_1.java
@@ -1,10 +1,13 @@
+package CodingBat;
+
 /**
- * Created by kseni on 23.06.2017.
+ * Created by kseni on 24.06.2017.
  */
-public class sleepIn {
+public class warmUp_1 {
     public boolean sleepIn(boolean weekday, boolean vacation) {
         if (!weekday || vacation)
             return true;
         else return false;
     }
+
 }
---
+++ src/CodingBat/warmUp_2.java
@@ -0,0 +1,7 @@
+package CodingBat;
+
+/**
+ * Created by kseni on 24.06.2017.
+ */
+public class warmUp_2 {
+}
